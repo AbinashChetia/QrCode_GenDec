cf1cfebc02fe02dc6ecc8c7fa45ff8db41c39728
Abinash Chetia <abinashdu2000@gmail.com>
2021-02-12 19:58:44 +0530

    Initial commit.

---
+++ qrCode.py
@@ -0,0 +1,29 @@
+# Author: Abinash Chetia
+# Dated: 12/02/2021
+
+import pyqrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
+from tkinter.filedialog import * # pylint: disable=unused-import
+
+opt = 0
+while opt != 3:
+    print("\nOptions:\n\t1. Generate QR code for input text.\n\t2. Decode QR code.\n\t3. Exit.")
+    opt = int(input("\nYour choice (1/2/3): "))
+
+    if opt == 1:
+        text = input("\nEnter message: ")
+        qr = pyqrcode.create(text)
+        savePath = asksaveasfilename()
+        qr.png(savePath+"_qr.png", scale=8)
+
+    elif opt == 2:
+        filePath = askopenfilename()
+        d = decode(Image.open(filePath))
+        print("\nDecoded message: "+d[0].data.decode("ascii"))
+
+    elif opt == 3:
+        print("\nExiting the program.\n")
+
+    else:
+        print("\nInvalid input.")
